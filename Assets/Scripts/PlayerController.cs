75ddeb77a66d00efc0ef85e38ff5e44287d10053
Alex Kopareiko <sanyakopar@gmail.com>
2022-05-01 17:15:07 +0300

    Lesson 3.3 - Don't Just Stand There https://learn.unity.com/tutorial/lesson-3-3-don-t-just-stand-there?uv=2020.3&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f7648a4edbc2a5578eb67df&projectId=5cf9639bedbc2a2b1fe1e848

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -14,16 +15,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && gameOver != true)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -34,8 +37,14 @@ public class PlayerController : MonoBehaviour
             isOnGround = true;
         } else if(collision.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("Game Over");
-            gameOver = true;
+            if(transform.position.y < collision.gameObject.GetComponent<BoxCollider>().size.y)
+            {
+                Debug.Log("Game Over");
+                gameOver = true;
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
+            }
+
         }
     }
 }
